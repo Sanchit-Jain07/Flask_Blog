3b9eca0059cfae89d0f8555cabe2bc82cf05fc99
Arnav Sharma <arnavsharma1702@gmail.com>
2020-06-28 17:55:23 +0530

    done

--- learn/config.py
+++ learn/config.py
@@ -8,7 +8,7 @@ class Config:
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
 	MAIL_USE_SSL = False
-	MAIL_USERNAME = 'testarnav17@gmail.com'
-	MAIL_PASSWORD = 'testmenow17'
+	MAIL_USERNAME = os.environ.get('EMAIL_USER')
+	MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 	MAIL_DEFAULT_SENDER = 'testarnav17@gmail.com'
 
